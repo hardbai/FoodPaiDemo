6b5840323fe817751162b36f6075d3ff76449142
hardbai <15736882756@163.com>
2016-07-12 19:18:46 +0800

    最终版本1

--- app/src/main/java/app/bai/com/foodpai/ui/ContentActivity.java
+++ app/src/main/java/app/bai/com/foodpai/ui/ContentActivity.java
@@ -3,12 +3,16 @@ package app.bai.com.foodpai.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import app.bai.com.foodpai.MyApp;
 import app.bai.com.foodpai.R;
@@ -59,5 +63,37 @@ public class ContentActivity extends AppCompatActivity {
         super.onDestroy();
         LoginActivity.removeHandler.sendEmptyMessage(1);
         MyApp.getApp().config.edit().putBoolean("isLogin",false).commit();
+    }
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // TODO Auto-generated method stub
+        if(keyCode == KeyEvent.KEYCODE_BACK)
+        {
+            exitBy2Click();      //调用双击退出函数
+        }
+        return false;
+    }
+    /**
+     * 双击退出函数
+     */
+    private static Boolean isExit = false;
+
+    private void exitBy2Click() {
+        Timer tExit = null;
+        if (isExit == false) {
+            isExit = true; // 准备退出
+            Toast.makeText(this, "再按一次退出程序", Toast.LENGTH_SHORT).show();
+            tExit = new Timer();
+            tExit.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    isExit = false; // 取消退出
+                }
+            }, 2000); // 如果2秒钟内没有按下返回键，则启动定时器取消掉刚才执行的任务
+
+        } else {
+            finish();
+            System.exit(0);
+        }
     }
 }
